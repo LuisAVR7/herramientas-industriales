5e61b4703dca7ed76245233b100adde33e42991b
Luis <luichivelazquez@gmail.com>
2026-07-21 10:50:00 -0300

    Fix: incluir carpeta content en el build de Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  outputFileTracingIncludes: {
+    "/blog": ["./content/**/*"],
+    "/blog/**/*": ["./content/**/*"],
+  },
 };
 
 export default nextConfig;
